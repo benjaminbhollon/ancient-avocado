486074d35b8b2e53c60536f1855f4a521767152b
Benjamin <benjaminbhollon@gmail.com>
2021-01-22 23:20:50 +0800

    Set up app

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 "use strict";
+
+//Import modules
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile('./app/');
+  win.maximize();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
