78f5f8be445935e3d9eae4c6d782b503c0c7e463
Benjamin <benjaminbhollon@gmail.com>
2021-01-24 21:24:38 +0800

    Add save feature for existing files

--- app/src/editor.js
+++ app/src/editor.js
@@ -5,11 +5,21 @@ var noSummaryMessage = "No summary provided. Click to add one.";
 var worldContent = {
   "title": "Untitled World"
 };
+var worldContentSaved = {};
 
 function populatePage() {
+  worldContentSaved = JSON.stringify(worldContent);
   document.getElementById("titleElement").innerText = worldContent.title + " | Verbose Guacamole";
   document.getElementById("title").innerText = worldContent.title;
   document.getElementById("summary").innerText = (worldContent.summary !== undefined && worldContent.summary.length > 0 ? worldContent.summary : noSummaryMessage);
+}
+
+async function save() {
+  if (filePath !== "") {
+    await fs.writeFile(filePath, JSON.stringify(worldContent), function (err, result) {
+      worldContentSaved = JSON.stringify(worldContent);
+    });
+  }
 }
 
 window.addEventListener("load", async function () {
@@ -23,6 +33,16 @@ window.addEventListener("load", async function () {
   }
 });
 
+document.getElementById("title").addEventListener("focus", function () {
+  if (this.innerText === "Untitled World") {
+    var selection = window.getSelection();
+    var range = document.createRange();
+    range.selectNodeContents(this);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
+});
+
 document.getElementById("summary").addEventListener("focus", function () {
   if (this.innerText === noSummaryMessage) {
     var selection = window.getSelection();
@@ -33,6 +53,15 @@ document.getElementById("summary").addEventListener("focus", function () {
   }
 });
 
+document.getElementById("title").addEventListener("input", function () {
+  worldContent.title = this.innerHTML;
+});
+
 document.getElementById("summary").addEventListener("input", function () {
   worldContent.summary = this.innerHTML;
 });
+
+setInterval(function () {
+  if (JSON.stringify(worldContent) == worldContentSaved) document.getElementById("saveButton").style.display = "none";
+  else document.getElementById("saveButton").style.display = "inline-block";
+}, 1000);
