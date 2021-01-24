15158ef64d87a0aa8312530e82aee0746e41559d
Benjamin <benjaminbhollon@gmail.com>
2021-01-24 20:58:29 +0800

    Add editable summary field.

--- app/src/editor.js
+++ app/src/editor.js
@@ -1,16 +1,38 @@
 const fs = require('fs');
-
 const filePath = (new URLSearchParams(window.location.search)).get('f');
+var noSummaryMessage = "No summary provided. Click to add one.";
+
 var worldContent = {
   "title": "Untitled World"
 };
+
+function populatePage() {
+  document.getElementById("titleElement").innerText = worldContent.title + " | Verbose Guacamole";
+  document.getElementById("title").innerText = worldContent.title;
+  document.getElementById("summary").innerText = (worldContent.summary !== undefined && worldContent.summary.length > 0 ? worldContent.summary : noSummaryMessage);
+}
 
 window.addEventListener("load", async function () {
   if (filePath !== null) {
     await fs.readFile(filePath, 'utf8', function (err, result) {
       worldContent = JSON.parse(result);
-      document.getElementById("title").innerText = worldContent.title + " | Verbose Guacamole";
+      populatePage();
     });
+  } else {
+    populatePage()
   }
-  console.log(worldContent);
+});
+
+document.getElementById("summary").addEventListener("focus", function () {
+  if (this.innerText === noSummaryMessage) {
+    var selection = window.getSelection();
+    var range = document.createRange();
+    range.selectNodeContents(this);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
+});
+
+document.getElementById("summary").addEventListener("input", function () {
+  worldContent.summary = this.innerHTML;
 });
